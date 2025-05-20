85ef2aedd6ab87e6e26aa0d906e69ec9e38f09d1
wikimodels <young.wiki.models@gmail.com>
2025-05-20 15:07:24 +0300

    first honorable commit

---
+++ functions/utility/redis/data-keys.mjs
@@ -0,0 +1,10 @@
+export const dataKeys = {
+  failedBybitPerpSymbols: "failedBybitPerpSymbols",
+  failedBybitSpotSymbols: "failedBybitSpotSymbols",
+  failedBinancePerpSymbols: "failedBinancePerpSymbols",
+  failedBinanceSpotSymbols: "failedBinanceSpotSymbols",
+  perp: "perp",
+  spot: "spot",
+  coins: "coins",
+  blacklist: "blacklist",
+};
